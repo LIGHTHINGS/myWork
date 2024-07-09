a26b78f905a38675eee540dd4d488a5c67c8bbea
LIGHTHINGS <dareomotayo32@gmail.com>
2024-07-09 18:11:38 +0100

    added the curl version

--- script.sh
+++ script.sh
@@ -3,10 +3,12 @@
 echo "hello from bash script"
 
 git_version=$(git --version | awk "{print $3}")
+curl_version=$(curl --version | grep curl)
 
 cat << EOF
 {
-	"GIT VERSION": $git_version
+	"GIT VERSION": $git_version,
+	"CURL_VERSION": $curl_version
 }
 EOF
 

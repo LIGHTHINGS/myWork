4154a59e9e917930e684295238d6ad89ee6a6e9f
LIGHTHINGS <dareomotayo32@gmail.com>
2024-07-10 15:49:27 +0100

    added httpd

--- script.sh
+++ script.sh
@@ -4,11 +4,12 @@ echo "hello from bash script"
 
 git_version=$(git --version | awk "{print $3}")
 curl_version=$(curl --version | grep curl)
-
+http_version=$(httpd -v | awk "{print $2}")
 cat << EOF
 {
 	"GIT VERSION": $git_version,
-	"CURL_VERSION": $curl_version
+	"CURL_VERSION": $curl_version,
+	"HTTPD_VERSION": $http_version
 }
 EOF
 
